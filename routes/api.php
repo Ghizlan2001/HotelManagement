d7711724f753b17c4f783f7bf65a6ab5fcc646b5
fatimazahraaboutaleb <fma87879@gmail.com>
2024-12-26 14:45:34 +0100

    add routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GuestController;
+use App\Http\Controllers\RoomController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\ServiceRequestController;
+use App\Http\Controllers\RoomMaintenanceController;
+use App\Http\Controllers\RoomTypeController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::apiResource('guests', GuestController::class);
+Route::apiResource('rooms', RoomController::class);
+Route::apiResource('reservations', ReservationController::class);
+Route::apiResource('payments', PaymentController::class);
+Route::apiResource('services', ServiceController::class);
+Route::apiResource('services-request', ServiceRequestController::class);
+Route::apiResource('room-maintenance', RoomMaintenanceController::class);
+Route::apiResource('room-types', RoomTypeController::class);
