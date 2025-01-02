6fbc616fc81f37f45248313c1266bd3d94797124
fatimazahraaboutaleb <fma87879@gmail.com>
2025-01-02 03:46:51 +0100

    added api in guests

---
+++ hotelreact/src/pages/guests.jsx
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const Guests=()=>{
+    const [Guests, setGuests]=useState([]);
+    useEffect(()=>{
+        getGuests();
+    },[]);
+    const getRooms=async()=>{
+        const resp = await axios.get("/guests");
+        setGuestss(resp.data);
+        console.log(resp.data)
+    }
+    getRooms();
+return(
+    <div>
+        <h6>Guests</h6>
+        <table>
+            <tr>
+                <th>Reservation Id</th>
+                <th>Name</th>
+                <th>Room Number</th>
+                <th>Total Amount</th>
+                <th>Status</th>
+            </tr>
+            {Guests.map((guest)=>(
+                guest.reservations.map((reservation) => (
+                    <tr key={reservation.id}>
+                        <td>{guest.first_name} {guest.last_name}</td>
+                        <td>{reservation.room_id}</td>
+                        <td>{reservation.total_amount}</td>
+                        <td>{reservation.reservation_status}</td>
+                    </tr>
+                ))
+            ))}
+        </table>
+    </div>
+);
+};
+export default Guests;
