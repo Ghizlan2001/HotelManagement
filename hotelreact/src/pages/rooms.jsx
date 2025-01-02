705c25824d89b6f9ccc74422a3d41ae4228418c2
Ghizlan2001 <gaboalm19@gmail.com>
2025-01-02 14:04:12 +0000

    added style

--- hotelreact/src/pages/rooms.jsx
+++ hotelreact/src/pages/rooms.jsx
@@ -12,44 +12,54 @@ const Rooms=()=>{
         console.log(resp.data)
     }
 return(
-    <div>
-        <h6>Room</h6>
-        <table border={2}>
-            <tr>
-                <th>Room number</th>
-                <th>Room type</th>
-                <th>Reservation status</th>
-                <th>Room status</th>
-                <th>Price per night</th>
-                <th>Payment status</th>
-                <th>Room maintenance</th>
-            </tr>
-            {
-                Rooms.map((room, index) => (
-            <tr key={index}>
-                <td>{room.room_number}</td>
-                <td>{room.room_type.room_type_name}</td>
+    <div className="container">
+        <h6 className="title">Room</h6>
+        <div className="content">
+           <div className="header">
+           <header>
+            <a href="" className="tab">All rooms</a>
+            <a href="" className="tab">Available rooms</a>
+            <a href="" className="tab">Booked rooms</a>
+            </header>
+            <button className="add">Add room</button>
+           </div>
+            <table>
+                <tr>
+                    <th>Room number</th>
+                    <th>Room type</th>
+                    <th>Reservation status</th>
+                    <th>Room status</th>
+                    <th>Price per night</th>
+                    <th>Payment status</th>
+                    <th>Room maintenance</th>
+                </tr>
+                {
+                    Rooms.map((room, index) => (
+                <tr key={index}>
+                    <td>{room.room_number}</td>
+                    <td>{room.room_type.room_type_name}</td>
+                    <td>
+                    {room.reservations && room.reservations.length > 0
+                        ? room.reservations[0].reservation_status
+                        : "No reservations"}
+                </td>
+                    <td>{room.room_status}</td>
+                    <td>{room.price_per_night}</td>
+                    <td>
+                    {room.reservations && room.reservations.length > 0
+                        ? room.reservations[0].payment_status
+                        : "No payment"}
+                </td>
                 <td>
-                {room.reservations && room.reservations.length > 0
-                    ? room.reservations[0].reservation_status
-                    : "No reservations"}
-            </td>
-                <td>{room.room_status}</td>
-                <td>{room.price_per_night}</td>
-                <td>
-                {room.reservations && room.reservations.length > 0
-                    ? room.reservations[0].payment_status
-                    : "No payment"}
-            </td>
-            <td>
-                {room.room_maintenance && room.room_maintenance.length > 0
-                    ? room.room_maintenance[0].maintenance_status // Display the status of the latest maintenance
-                    : "No maintenance"}
-            </td>
-            </tr>
-        ))
-}
-        </table>
+                    {room.room_maintenance && room.room_maintenance.length > 0
+                        ? room.room_maintenance[0].maintenance_status // Display the status of the latest maintenance
+                        : "No maintenance"}
+                </td>
+                </tr>
+            ))
+    }
+            </table>
+        </div>
     </div>
 );
 };export default Rooms;
