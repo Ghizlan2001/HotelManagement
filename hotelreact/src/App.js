a36fe04c9538ad1e85a4ba06808f7678ed9c6c32
fatimazahraaboutaleb <fma87879@gmail.com>
2025-01-09 01:12:29 +0100

    added routes and sidebar

--- hotelreact/src/App.js
+++ hotelreact/src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
-// import {Router, Routes, Route } from "react-router-dom";
 import Rooms from './pages/rooms';
 import Guests from './pages/guests';
+import Sidebar from './pages/sidebar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Rooms />
-      <Guests />
+      <BrowserRouter>
+        <Sidebar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/rooms" element={<Rooms />} />
+            <Route path="/guests" element={<Guests />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
